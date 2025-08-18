094c1f79eb4410af1c09e132d51b216000cefb27
Jagannath Ayinipully <jayi0001@student.monash.edu>
2025-08-18 12:12:29 +1000

    - Customised and simplified Contact page. - Added marked as replied action and removed edit action with error and success handling. - Changed date locale in app.php - Added mark as replied function in FormsController.php - Changed header links and naming. - Changed email address in view to be a mailto: link.

--- tests/Fixture/FormsFixture.php
+++ tests/Fixture/FormsFixture.php
@@ -19,12 +19,13 @@ class FormsFixture extends TestFixture
     {
         $this->records = [
             [
-                'id' => '',
+                'id' => '83a80554-76e1-4148-b090-6a92b1def73f',
                 'first_name' => 'Lorem ipsum dolor sit amet',
                 'last_name' => 'Lorem ipsum dolor sit amet',
                 'email' => 'Lorem ipsum dolor sit amet',
                 'message' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-                'created' => 1755396679,
+                'replied_to' => 1,
+                'date_created' => 1755479914,
             ],
         ];
         parent::init();
